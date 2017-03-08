fab0f1dc4c8f87005b4cc63123470b546a366ec9
Barney Carroll <barney.carroll@gmail.com>
2017-03-08 11:49:30 +0000

    Refactor core lifecycle overrides, fix low-level bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import m from 'mithril'
+// import m from 'mithril'
 
 // The supplementary model for determining a/sync diff-exclusion state
 const stopped = new Set() // False return, abort in current loop
@@ -10,16 +10,16 @@ const unmade  = new Set() // Failed creation
 // Convenience function to fork logic depending on method return values
 const fork    = (outcome, ifBoth, ifAsync, ifFalse, ifNone) => {
   const isFalse = outcome == false
-  const isAsync = typeof outcome.then === 'function'
+  const isAsync = outcome && typeof outcome.then === 'function'
 
        if(isFalse) ifFalse(outcome)
   else if(isAsync) ifAsync(outcome)
-  else return void ifNone(outcome)
+  else return void ifNone ? ifNone(outcome) : undefined
                    ifBoth(outcome)
 }
 
-// A dummy component definition to use as a placeholder and force diffs on uninitialised components
-const dummy   = {view:() => []}
+const call    = (method, vnode, ...rest) =>
+  method ? method.call(vnode.state, vnode, ...rest) : undefined
 
 // Shorthands for terse state-keeping
 const setter  = set =>
@@ -34,144 +34,123 @@ const setter  = set =>
   }
 const wait    = setter(waiting)
 const stop    = setter(stopped)
-const aint    = setter(isntit) /* || function(vnode, remove){
-  if(arguments.length === 1){
-    return isntit.has(vnode.state)
-  }
-  else if(remove)
-    isntit.delete(vnode.state)
-    x.tag = x.state.view
-  }
-  else {
-    isntit.add(x.state)
-    x.tag = dummy
-  }
-}*/
+const aint    = setter(isntit)
 const unmake  = setter(unmade)
 
-export default function asynchronise(component){
-  const {view, oninit, oncreate, onbeforeupdate, onupdate} = component
-  const returned = Object.assign({}, component)
+/* export default */ function asynchronise(component){
+  const override = {
+    view: vnode => (
+      aint(vnode)
+      ? []
+      : call(component.view, vnode)
+    ),
 
-  if(view)
-    returned.view = function(vnode){
-      return isnt(vnode) ? [] : view.apply(this, arguments)
-    }
+    oninit: vnode => (
+      wait(vnode)
+      ? undefined
+      : !!fork(
+          call(component.oninit, vnode),
 
-  if(oninit)
-    returned.oninit = function(vnode){
-      if(wait(vnode))
-        return
+          () => {
+            aint(vnode, true)
+          },
 
-      return !!fork(
-        oninit.apply(this, arguments),
+          () => {
+            stop(vnode, true)
 
-        () => { aint(vnode, true) },
+            asap().then(() => {
+              stop(vnode, false)
+            })
+          },
 
-        () => {
-          stop(vnode, true)
+          promise => {
+            wait(vnode, true)
 
-          asap().then(() => {
+            promise.then(() => {
+              wait(vnode, false)
+              aint(vnode, false)
+
+              m.redraw()
+            })
+          },
+
+          () => {
             stop(vnode, false)
-          })
-        },
-
-        promise => {
-          wait(vnode, true)
-
-          promise.then(() => {
-            wait(vnode, false)
             aint(vnode, false)
+          }
+        )
+    ),
 
-            m.redraw()
-          })
-        },
+    oncreate : vnode => (
+      ( wait(vnode) || stop(vnode) )
+      ? false
+      : fork(
+          call(component.oncreate, vnode),
 
-        () => {
-          stop(vnode, false)
-          aint(vnode, false)
-        }
-      )
-    }
+          () => {
+            unmake(vnode, true)
+          },
 
-  if(oncreate)
-    returned.oncreate = function(vnode){
-      if(wait(vnode) || stop(vnode))
-        return false
+          Function.prototype,
 
-      return fork(
-        oncreate.apply(this, arguments),
+          promise => {
+            wait(vnode, true)
 
-        () => {
-          unmake(vnode, true)
-        },
+            promise.then(() => {
+              wait(vnode, false)
+            })
+          }
+        )
+    ),
 
-        Function.prototype,
+    onbeforeupdate : (vnode, old) => (
+      wait(vnode)
+      ? false
+      : aint(vnode)
+        ? call(override.oninit, vnode, old)
+        : !!fork(
+            call(component.onbeforeupdate, vnode, old),
 
-        promise => {
-          wait(vnode, true)
+            () => false,
 
-          promise.then(() => {
-            wait(vnode, false)
-          })
-        }
-      )
-    }
+            () => false,
 
-  if(onbeforeupdate)
-    returned.onbeforeupdate = function(vnode){
-      if(wait(vnode))
-        return false
+            promise => {
+              wait(vnode, true)
 
-      if(aint(vnode))
-        return returned.oninit.apply(this, arguments)
+              promise.then(() => {
+                wait(vnode, false)
 
-      return !!fork(
-        onbeforeupdate.apply(this, arguments),
+                m.redraw()
+              })
+            }
+          )
+    ),
 
-        () => false,
+    onupdate : vnode => (
+      wait(vnode)
+      ? false
+      : unmake(vnode)
+        ? call(override.oncreate, vnode)
+        : fork(
+            call(component.onupdate, vnode),
 
-        () => false,
+            Function.prototype,
 
-        promise => {
-          wait(vnode, true)
+            Function.prototype,
 
-          promise.then(() => {
-            wait(vnode, false)
+            promise => {
+              wait(vnode, true)
 
-            m.redraw()
-          })
-        }
-      )
-    }
+              promise.then(() => {
+                wait(vnode, false)
 
-  if(onupdate)
-    returned.onbeforeupdate = function(vnode){
-      if(wait(vnode))
-        return false
+                m.redraw()
+              })
+            }
+          )
+    ),
+  }
 
-      if(aint(vnode))
-        return returned.oncreate.apply(this,arguments)
-
-      if(unmake(vnode))
-        return returned.oncreate.apply(this, arguments)
-
-      return fork(
-        onbeforeupdate.apply(this, arguments),
-
-        Function.prototype,
-
-        Function.prototype,
-
-        promise => {
-          wait(vnode, true)
-
-          promise.then(() => {
-            wait(vnode, false)
-
-            m.redraw()
-          })
-        }
-      )
-    }
+  return Object.assign({}, component, override)
 }
